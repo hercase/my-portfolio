f4fc26f44a5958c63b73c2d31b25b1b1e6f7d78f
Hernan Case <her.case@live.com.ar>
2021-05-13 23:54:34 -0300

    Starting Project

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="title">HERNANCASE.COM</h1>
+    </div>
+  );
+};
+
+export default Header;
